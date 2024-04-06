aa9253ec5ee4513c363fdbcbf4d7f06d6811cd0d
Carlos Eduardo Batista <carlosedbat@gmail.com>
2024-04-06 18:40:31 -0300

    feat:modificado a view principal com organizacao de paineis

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -38,41 +38,58 @@
             btnSend = new Button();
             groupBox1 = new GroupBox();
             labelATM = new Label();
-            groupBox2 = new GroupBox();
             labelConcentracaoN2 = new Label();
             groupBox3 = new GroupBox();
             labelMassaDeNitrogenio = new Label();
             groupBox4 = new GroupBox();
             labelPorcentagemN2 = new Label();
+            buttonStart = new Button();
+            buttonStop = new Button();
+            label1 = new Label();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            panel4 = new Panel();
+            labelConnectionStatus = new Label();
+            labelReadingStatus = new Label();
+            label3 = new Label();
+            label4 = new Label();
             ((System.ComponentModel.ISupportInitialize)bindingSource1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)bindingSource2).BeginInit();
             groupBox1.SuspendLayout();
-            groupBox2.SuspendLayout();
             groupBox3.SuspendLayout();
             groupBox4.SuspendLayout();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel4.SuspendLayout();
             SuspendLayout();
             // 
             // txtTerminal
             // 
-            txtTerminal.Location = new Point(330, 379);
+            txtTerminal.Location = new Point(206, 274);
             txtTerminal.Multiline = true;
             txtTerminal.Name = "txtTerminal";
             txtTerminal.ScrollBars = ScrollBars.Vertical;
-            txtTerminal.Size = new Size(590, 178);
+            txtTerminal.Size = new Size(720, 178);
             txtTerminal.TabIndex = 0;
             // 
             // txtCommand
             // 
-            txtCommand.Location = new Point(101, 534);
+            txtCommand.Enabled = false;
+            txtCommand.Location = new Point(23, 534);
             txtCommand.Name = "txtCommand";
-            txtCommand.Size = new Size(208, 23);
+            txtCommand.Size = new Size(161, 23);
             txtCommand.TabIndex = 1;
             // 
             // btnConnect
             // 
-            btnConnect.Location = new Point(12, 57);
+            btnConnect.Location = new Point(12, 174);
             btnConnect.Name = "btnConnect";
-            btnConnect.Size = new Size(110, 23);
+            btnConnect.Size = new Size(172, 23);
             btnConnect.TabIndex = 2;
             btnConnect.Text = "Conectar";
             btnConnect.UseVisualStyleBackColor = true;
@@ -81,16 +98,17 @@
             // cmbSerialPort
             // 
             cmbSerialPort.FormattingEnabled = true;
-            cmbSerialPort.Location = new Point(144, 57);
+            cmbSerialPort.Location = new Point(12, 135);
             cmbSerialPort.Name = "cmbSerialPort";
-            cmbSerialPort.Size = new Size(165, 23);
+            cmbSerialPort.Size = new Size(172, 23);
             cmbSerialPort.TabIndex = 3;
             // 
             // btnSend
             // 
-            btnSend.Location = new Point(12, 532);
+            btnSend.Enabled = false;
+            btnSend.Location = new Point(23, 503);
             btnSend.Name = "btnSend";
-            btnSend.Size = new Size(83, 25);
+            btnSend.Size = new Size(161, 25);
             btnSend.TabIndex = 4;
             btnSend.Text = "Enviar";
             btnSend.UseVisualStyleBackColor = true;
@@ -98,7 +116,7 @@
             // groupBox1
             // 
             groupBox1.Controls.Add(labelATM);
-            groupBox1.Location = new Point(330, 25);
+            groupBox1.Location = new Point(83, 12);
             groupBox1.Name = "groupBox1";
             groupBox1.Size = new Size(143, 68);
             groupBox1.TabIndex = 5;
@@ -108,35 +126,27 @@
             // labelATM
             // 
             labelATM.AutoSize = true;
-            labelATM.Location = new Point(60, 32);
+            labelATM.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            labelATM.Location = new Point(30, 23);
             labelATM.Name = "labelATM";
-            labelATM.Size = new Size(22, 15);
+            labelATM.Size = new Size(84, 30);
             labelATM.TabIndex = 9;
-            labelATM.Text = "0.0";
-            // 
-            // groupBox2
-            // 
-            groupBox2.Controls.Add(labelConcentracaoN2);
-            groupBox2.Location = new Point(479, 25);
-            groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(143, 68);
-            groupBox2.TabIndex = 6;
-            groupBox2.TabStop = false;
-            groupBox2.Text = "Concentração de N2";
+            labelATM.Text = "0.00000";
             // 
             // labelConcentracaoN2
             // 
             labelConcentracaoN2.AutoSize = true;
-            labelConcentracaoN2.Location = new Point(62, 32);
+            labelConcentracaoN2.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            labelConcentracaoN2.Location = new Point(30, 12);
             labelConcentracaoN2.Name = "labelConcentracaoN2";
-            labelConcentracaoN2.Size = new Size(22, 15);
+            labelConcentracaoN2.Size = new Size(84, 30);
             labelConcentracaoN2.TabIndex = 10;
-            labelConcentracaoN2.Text = "0.0";
+            labelConcentracaoN2.Text = "0.00000";
             // 
             // groupBox3
             // 
             groupBox3.Controls.Add(labelMassaDeNitrogenio);
-            groupBox3.Location = new Point(628, 25);
+            groupBox3.Location = new Point(381, 12);
             groupBox3.Name = "groupBox3";
             groupBox3.Size = new Size(143, 68);
             groupBox3.TabIndex = 7;
@@ -146,16 +156,17 @@
             // labelMassaDeNitrogenio
             // 
             labelMassaDeNitrogenio.AutoSize = true;
-            labelMassaDeNitrogenio.Location = new Point(59, 32);
+            labelMassaDeNitrogenio.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            labelMassaDeNitrogenio.Location = new Point(27, 23);
             labelMassaDeNitrogenio.Name = "labelMassaDeNitrogenio";
-            labelMassaDeNitrogenio.Size = new Size(22, 15);
+            labelMassaDeNitrogenio.Size = new Size(84, 30);
             labelMassaDeNitrogenio.TabIndex = 11;
-            labelMassaDeNitrogenio.Text = "0.0";
+            labelMassaDeNitrogenio.Text = "0.00000";
             // 
             // groupBox4
             // 
             groupBox4.Controls.Add(labelPorcentagemN2);
-            groupBox4.Location = new Point(777, 25);
+            groupBox4.Location = new Point(530, 12);
             groupBox4.Name = "groupBox4";
             groupBox4.Size = new Size(143, 68);
             groupBox4.TabIndex = 8;
@@ -165,25 +176,152 @@
             // labelPorcentagemN2
             // 
             labelPorcentagemN2.AutoSize = true;
-            labelPorcentagemN2.Location = new Point(62, 32);
+            labelPorcentagemN2.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            labelPorcentagemN2.Location = new Point(26, 23);
             labelPorcentagemN2.Name = "labelPorcentagemN2";
-            labelPorcentagemN2.Size = new Size(22, 15);
+            labelPorcentagemN2.Size = new Size(96, 30);
             labelPorcentagemN2.TabIndex = 12;
-            labelPorcentagemN2.Text = "0.0";
+            labelPorcentagemN2.Text = "000.00 %";
+            // 
+            // buttonStart
+            // 
+            buttonStart.Location = new Point(12, 239);
+            buttonStart.Name = "buttonStart";
+            buttonStart.Size = new Size(75, 23);
+            buttonStart.TabIndex = 9;
+            buttonStart.Text = "Iniciar";
+            buttonStart.UseVisualStyleBackColor = true;
+            // 
+            // buttonStop
+            // 
+            buttonStop.Location = new Point(109, 239);
+            buttonStop.Name = "buttonStop";
+            buttonStop.Size = new Size(75, 23);
+            buttonStop.TabIndex = 10;
+            buttonStop.Text = "Parar";
+            buttonStop.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(14, 44);
+            label1.Name = "label1";
+            label1.Size = new Size(115, 15);
+            label1.TabIndex = 12;
+            label1.Text = "Concentração de N2";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(labelConcentracaoN2);
+            panel1.Location = new Point(235, 13);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(140, 67);
+            panel1.TabIndex = 13;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Controls.Add(buttonStop);
+            panel2.Controls.Add(cmbSerialPort);
+            panel2.Controls.Add(buttonStart);
+            panel2.Controls.Add(btnConnect);
+            panel2.Controls.Add(txtCommand);
+            panel2.Controls.Add(btnSend);
+            panel2.Dock = DockStyle.Left;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(200, 569);
+            panel2.TabIndex = 14;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(groupBox1);
+            panel3.Controls.Add(groupBox3);
+            panel3.Controls.Add(panel1);
+            panel3.Controls.Add(groupBox4);
+            panel3.Dock = DockStyle.Top;
+            panel3.Location = new Point(200, 0);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(738, 86);
+            panel3.TabIndex = 15;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(172, 74);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(54, 111);
+            label2.Name = "label2";
+            label2.Size = new Size(89, 21);
+            label2.TabIndex = 4;
+            label2.Text = "Porta Serial";
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(label4);
+            panel4.Controls.Add(label3);
+            panel4.Controls.Add(labelReadingStatus);
+            panel4.Controls.Add(labelConnectionStatus);
+            panel4.Dock = DockStyle.Bottom;
+            panel4.Location = new Point(200, 534);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(738, 35);
+            panel4.TabIndex = 16;
+            // 
+            // labelConnectionStatus
+            // 
+            labelConnectionStatus.AutoSize = true;
+            labelConnectionStatus.Location = new Point(126, 11);
+            labelConnectionStatus.Name = "labelConnectionStatus";
+            labelConnectionStatus.Size = new Size(82, 15);
+            labelConnectionStatus.TabIndex = 0;
+            labelConnectionStatus.Text = "Desconectado";
+            // 
+            // labelReadingStatus
+            // 
+            labelReadingStatus.AutoSize = true;
+            labelReadingStatus.Location = new Point(630, 11);
+            labelReadingStatus.Name = "labelReadingStatus";
+            labelReadingStatus.Size = new Size(82, 15);
+            labelReadingStatus.TabIndex = 1;
+            labelReadingStatus.Text = "Desconectado";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(6, 11);
+            label3.Name = "label3";
+            label3.Size = new Size(105, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Status da Conexão";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(517, 11);
+            label4.Name = "label4";
+            label4.Size = new Size(94, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Status da Leitura";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(938, 569);
-            Controls.Add(groupBox4);
-            Controls.Add(groupBox3);
-            Controls.Add(groupBox2);
-            Controls.Add(groupBox1);
-            Controls.Add(btnSend);
-            Controls.Add(cmbSerialPort);
-            Controls.Add(btnConnect);
-            Controls.Add(txtCommand);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
             Controls.Add(txtTerminal);
             Name = "Form1";
             Text = "Form1";
@@ -192,12 +330,18 @@
             ((System.ComponentModel.ISupportInitialize)bindingSource2).EndInit();
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
-            groupBox2.ResumeLayout(false);
-            groupBox2.PerformLayout();
             groupBox3.ResumeLayout(false);
             groupBox3.PerformLayout();
             groupBox4.ResumeLayout(false);
             groupBox4.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -212,12 +356,24 @@
         private ComboBox cmbSerialPort;
         private Button btnSend;
         private GroupBox groupBox1;
-        private GroupBox groupBox2;
         private Label labelATM;
         private Label labelConcentracaoN2;
         private GroupBox groupBox3;
         private Label labelMassaDeNitrogenio;
         private GroupBox groupBox4;
         private Label labelPorcentagemN2;
+        private Button buttonStart;
+        private Button buttonStop;
+        private Label label1;
+        private Panel panel1;
+        private Panel panel2;
+        private Label label2;
+        private PictureBox pictureBox1;
+        private Panel panel3;
+        private Panel panel4;
+        private Label label4;
+        private Label label3;
+        private Label labelReadingStatus;
+        private Label labelConnectionStatus;
     }
 }
