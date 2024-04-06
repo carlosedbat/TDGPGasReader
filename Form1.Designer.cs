09747667905616c18354aca13ff3acb4e8ad9ebe
Carlos Eduardo Batista <carlosedbat@gmail.com>
2024-04-06 14:29:12 -0300

    feat:primeira comunicação realizada

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            bindingSource1 = new BindingSource(components);
+            bindingSource2 = new BindingSource(components);
+            txtTerminal = new TextBox();
+            txtCommand = new TextBox();
+            btnConnect = new Button();
+            cmbSerialPort = new ComboBox();
+            btnSend = new Button();
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)bindingSource2).BeginInit();
             SuspendLayout();
+            // 
+            // txtTerminal
+            // 
+            txtTerminal.Location = new Point(330, 57);
+            txtTerminal.Multiline = true;
+            txtTerminal.Name = "txtTerminal";
+            txtTerminal.ScrollBars = ScrollBars.Vertical;
+            txtTerminal.Size = new Size(604, 436);
+            txtTerminal.TabIndex = 0;
+            // 
+            // txtCommand
+            // 
+            txtCommand.Location = new Point(101, 470);
+            txtCommand.Name = "txtCommand";
+            txtCommand.Size = new Size(208, 23);
+            txtCommand.TabIndex = 1;
+            // 
+            // btnConnect
+            // 
+            btnConnect.Location = new Point(12, 57);
+            btnConnect.Name = "btnConnect";
+            btnConnect.Size = new Size(110, 23);
+            btnConnect.TabIndex = 2;
+            btnConnect.Text = "Conectar";
+            btnConnect.UseVisualStyleBackColor = true;
+            btnConnect.Click += btnConnect_Click;
+            // 
+            // cmbSerialPort
+            // 
+            cmbSerialPort.FormattingEnabled = true;
+            cmbSerialPort.Location = new Point(144, 57);
+            cmbSerialPort.Name = "cmbSerialPort";
+            cmbSerialPort.Size = new Size(165, 23);
+            cmbSerialPort.TabIndex = 3;
+            // 
+            // btnSend
+            // 
+            btnSend.Location = new Point(12, 468);
+            btnSend.Name = "btnSend";
+            btnSend.Size = new Size(83, 25);
+            btnSend.TabIndex = 4;
+            btnSend.Text = "Enviar";
+            btnSend.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(946, 569);
+            Controls.Add(btnSend);
+            Controls.Add(cmbSerialPort);
+            Controls.Add(btnConnect);
+            Controls.Add(txtCommand);
+            Controls.Add(txtTerminal);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)bindingSource2).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private BindingSource bindingSource1;
+        private BindingSource bindingSource2;
+        private TextBox txtTerminal;
+        private TextBox txtCommand;
+        private Button btnConnect;
+        private ComboBox cmbSerialPort;
+        private Button btnSend;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,92 @@
 namespace TDGPGasReader
 {
+    using System.IO.Ports;
+
     public partial class Form1 : Form
     {
+        private SerialPort serialPort1;
+
         public Form1()
         {
             InitializeComponent();
+            serialPort1 = new System.IO.Ports.SerialPort(components);
+            SetSerialPort();
+            InitializeSerialPort();
+            PopulateSerialPortComboBox();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SetSerialPort()
+        {
+            serialPort1.PortName = "COM9";
+            serialPort1.BaudRate = 19200;
+            serialPort1.Parity = Parity.None;
+            serialPort1.DataBits = 8;
+            serialPort1.StopBits = StopBits.One;
+            serialPort1.Handshake = Handshake.None;
+        }
+
+        private void InitializeSerialPort()
+        {
+            serialPort1.DataReceived += SerialPort1_DataReceived;
+        }
+
+        private void PopulateSerialPortComboBox()
+        {
+            cmbSerialPort.Items.Clear();
+            foreach (string s in System.IO.Ports.SerialPort.GetPortNames())
+            {
+                cmbSerialPort.Items.Add(s);
+            }
+            if (cmbSerialPort.Items.Count > 0) cmbSerialPort.SelectedIndex = 0;
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.Close();
+                    btnConnect.Text = "Connect";
+                }
+                else
+                {
+                    serialPort1.PortName = cmbSerialPort.SelectedItem.ToString();
+                    serialPort1.Open();
+                    btnConnect.Text = "Disconnect";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.WriteLine(txtCommand.Text);
+                txtCommand.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Serial port is not connected.");
+            }
+        }
+
+        private void SerialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            string data = serialPort1.ReadExisting();
+            Invoke(new MethodInvoker(delegate
+            {
+                txtTerminal.AppendText(data);
+            }));
         }
     }
 }
